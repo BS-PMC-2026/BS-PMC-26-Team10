a36b252ecff02f93e21b9250864d01ce9023394e
nathanwashere_ <nathano@ac.sce.ac.il>
2026-06-01 15:42:26 +0300

    updated for CI

--- Backend/app/db2.py
+++ Backend/app/db2.py
@@ -10,7 +10,7 @@ load_dotenv()
 SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_KEY = os.getenv('SUPABASE_KEY')
 
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY) if (SUPABASE_URL and SUPABASE_KEY) else None
 
 
 def get_chilleis():
